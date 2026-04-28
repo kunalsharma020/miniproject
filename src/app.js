19330f4c7a426ecbd90d1003e15a3ddf8549aa66
kirtikagoel <kirtigoel2004@gmail.com>
2026-04-28 09:13:52 +0530

    Added features

--- src/app.js
+++ src/app.js
@@ -14,12 +14,14 @@ const { errorHandler } = require('./middleware/errorHandler');
 const configRoutes = require('./routes/configRoutes');
 const schemaRoutes = require('./routes/schemaRoutes');
 
+
 const app = express();
 
 app.disable('x-powered-by');
 app.use(cors());
 app.use(helmet());
 app.use(express.json({ limit: '1mb' }));
+
 
 app.use(
   morgan('combined', {
